84e430e8a4f6259c1a970588ef555960fba3c7c4
Rennan Oliveira <rennan_neves@live.com>
2020-04-14 15:34:55 -0300

    response code

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    function reducer(accumulator: number, currentValue: Transaction): number {
+      return accumulator + currentValue.value;
+    }
+
+    const incomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const incomeTotal = incomeTransactions.reduce(reducer, 0);
+
+    const outcomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+    const outcomeTotal = outcomeTransactions.reduce(reducer, 0);
+
+    const balance: Balance = {
+      income: incomeTotal,
+      outcome: outcomeTotal,
+      total: incomeTotal - outcomeTotal,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
